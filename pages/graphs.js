e37cc22f374201efb1e61418f0f24d909fb39ec4
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2022-11-02 14:54:18 -0400

    init push

--- pages/graphs.js
+++ pages/graphs.js
@@ -1,24 +1,24 @@
+import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import React from 'react'
-import Head from 'next/head'
 
-const DynamicPlot = dynamic(import('./line'), {
-    ssr: false
-  })
-  
-const DynamicPlot2 = dynamic(import('./bar'), {
-    ssr: false
-  })  
-  
-  export default function Home() {
-    return (
-      <React.Fragment>
-        <Head>
-          <title>Next Plotly</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <DynamicPlot />
-        <DynamicPlot2 />
-      </React.Fragment>
-    )
-  }
+const LineGraph = dynamic(import('./line'), {
+  ssr: false
+})
+
+const BarGraph = dynamic(import('./bar'), {
+  ssr: false
+})
+
+export default () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Next Plotly</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LineGraph />
+      <BarGraph />
+    </React.Fragment>
+  )
+}
