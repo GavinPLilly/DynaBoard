41c679b83a3280641d2ae37a57e8ce14c74d1fb7
Gavin Lilly <gavinlilly25@gmail.com>
2022-12-18 20:54:38 -0500

    Fixed import pathname typos

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../styles/index.module.css'
 import Image from 'next/image'
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from '../Components/Header'
 
 function index() {
   return (
