cde7fcca7f75bcd3d885498d2ce737d6bcdc24b8
jaythk27 <75706845+jaythk27@users.noreply.github.com>
2022-12-19 01:36:24 -0500

    Create dummy-data.js

---
+++ pages/api/GET/dummy-data.js
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = 'https://uxugmioardnyredowtry.supabase.co'
+const supabaseKey = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4dWdtaW9hcmRueXJlZG93dHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYxOTk2ODQsImV4cCI6MTk4MTc3NTY4NH0.zUqJvrgCMTd-eem_DVaP249mUNYzCftwAv2zbwXBM4k'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default async function handler(req, res) {
+
+    let { data: groups, error } = await supabase
+        .from('dummy-data')
+        .select('*')
+
+    res.status(200).json(groups)
+}
